f9b68a84bf258b90567ebe49e28e22674b1b19ad
gvazidra <162367702+gvazidra@users.noreply.github.com>
2025-03-31 22:25:13 +0000

    final

--- src/main.c
+++ src/main.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
 #include "hello.h"
 
-int main() {
+int main(void) {
     print_hello();
     return 0;
-}/* hello.c */
-#include <stdio.h>
-
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
 }
